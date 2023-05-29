1b1d4b433e88fe7864b0879c4847d1bcb0caec73
Dolores Crazover <doloresnetweb@gmail.com>
2023-05-28 22:44:24 -0400

    fixe some errors

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,47 +2,46 @@
 const express = require("express")
 const { User } = require("../models/User")
 const bcrypt = require("bcrypt")
-const Joi = require("joi");
-const { createUserToken } = require("../middleware/auth");
-const router = express.Router(); // Create a new router
-
+const Joi = require("joi")
+const { createUserToken } = require("../middleware/auth")
+const router = express.Router() // Create a new router
 
 // POST /auth/login
 router.post("/", async (req, res) => {
-    try {
+  console.log(approve(req.body))
+  try {
     // Validate the request body with the approve function imported from User model
-    const { error } = approve(req.body);
+    const { error } = approve(req.body)
     // If there's an error in validation, send a 400 status code with error message
-    if (error) return res.status(400).send({message:error.details[0].message});
+    if (error)
+      return res.status(400).send({ message: error.details[0].message })
 
     // if absence of error, check if the user with the same email address already exist
-    const user = await User.findOne({email:req.body.email});
-// If user does not exist, send a 401 status code with an error message
-if (!user)
-return res.status(401).send({message:"Invalid password or email"});
+    const user = await User.findOne({ email: req.body.email })
+    // If user does not exist, send a 401 status code with an error message
+    if (!user)
+      return res.status(401).send({ message: "Invalid password or email" })
 
-const validPassword = await bcrypt.compare(
-    req.body.password, user.password
-);
-if (!validPassword)
-            return res.status(401).send("Invalid email or password");
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
+    if (!validPassword) return res.status(401).send("Invalid email or password")
 
-            // If email and password are valide generate a token
-            const userToken = user.generateAuthToken();
-            res.status(200).send({data:userToken, message: "Logged in successfully"})
-        } catch (error) {
-            res.status(500).send({message:"An error occured with the internal server"});
-        }
-    });
-
+    // If email and password are valide generate a token
+    const userToken = user.generateAuthToken()
+    res.status(200).send({ data: userToken, message: "Logged in successfully" })
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: "An error occured with the internal server" })
+  }
+})
 
 const approve = (userData) => {
-    const schema = Joi.object({
-        email: Joi.string().email().required().label("Email"),
-        password: Joi.string().required().label("Password"),
-    });
-    return schema.approve(userData);
-};
+  const schema = Joi.object({
+    email: Joi.string().email().required().label("Email"),
+    password: Joi.string().required().label("Password"),
+  })
+  return schema.validate(userData)
+}
 
 // Export the router to be used in the main server file
-module.exports = router;
+module.exports = router
--- server.js
+++ server.js
@@ -1,31 +1,32 @@
-require("dotenv").config();
-const express = require('express');
-const app = express();
-const cors = require("cors");
-const mongoose = require('mongoose');
-const methodOverride = require('method-override');
-const PORT = 4000;
+require("dotenv").config()
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const mongoose = require("mongoose")
+const methodOverride = require("method-override")
+const PORT = 4000
 
 // Require the user resource routes and controllers
-const userController = require("./controllers/users");
-const authController = require("./controllers/auth");
-
+const userController = require("./controllers/users")
+const authController = require("./controllers/auth")
 
 //Middlewares
-app.use(express.json()); // help to submit the data from page to page with form
-app.use(cors());
-app.use(express.urlencoded({ extended: true}));
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
-app.use(methodOverride('_method'));
+app.use(express.json()) // help to submit the data from page to page with form
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static("public"))
+app.set("view engine", "ejs")
+app.use(methodOverride("_method"))
 
 // Use the auth routes
-app.use('/api/users', authController);
-app.use('/api/auth', authController);
+app.use("/api/users", userController)
+app.use("/api/auth", authController)
 
-app.get('/', (req, res) => {
-    res.send('default route - functional');
-});
+app.get("/", (req, res) => {
+  res.send("default route - functional")
+})
 
 //Listener
-app.listen(PORT, () => console.log(`Listening for client requests on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Listening for client requests on port ${PORT}`)
+)
